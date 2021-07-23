a11dc9654a63b6f314196f9ea45835f5c992ac2b
NamrathaNShetty <namratanshetty@gmail.com>
2021-07-23 20:50:43 +0530

    Added: Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,27 +1,29 @@
 #!/bin/bash -x
 isparttime=1;
 isfulltime=2;
-max_Hrs_Mon=10;
+max_Hrs_Mon=4;
 emprateperHr=20;
 numworkingdays=20;
 totalEmpHr=0;
 totalworkingdays=0;
-while [[ $totalEmpHr -lt $max_Hrs_Mon &&
-	 $totalworkingdays -lt $numworkingdays ]]
-do
-  ((totalworkingdays++))
-	empcheck=$(( RANDOM%3 ))
-        case $empcheck in
+function getworkinghrs() {
+ case $1 in
                 $isfulltime)
                         empHrs=8
                                 ;;
                 $isparttime)
                         empHrs=4
                                 ;;
-                *)
-                        empHrs=0
-                                ;;
+		*)
+			empHrs=0
+				;;
         esac
-	totalEmpHr=$(($totalEmpHr*$empHrs))
+}
+while [[ $totalworkingHrs -lt $max_Hrs_Mon &&
+	 $totalworkingdays -lt $numworkingdays ]]
+do
+  ((totalworkingdays++))
+	empHrs="$( getworkinghrs $((RANDOM%3)) )"
+	totalworkingHrs=$(( totalworkingHrs * empHrs ))
 done
-totalSalary=$(($totalEmpHr*$emprateperHr))
+totalSalary=$(( totalworkingHrs * emprateperHr ))
