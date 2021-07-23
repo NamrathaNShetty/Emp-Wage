8d945abca4c3d1f34793c0a3c0a1f850d0783211
NamrathaNShetty <namratanshetty@gmail.com>
2021-07-23 19:54:23 +0530

    Added: switchCase

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -x
 
-
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-empcheck=$((RANDOM%3))
+empCheck=$((RANDOM%3))
 
-if [[ $isFullTime -eq $empCheck ]]
-then
-	empHrs=8;
-elif [[ $isPartTime -eq $empCheck ]]
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
-
-salary=$(($empHrs*$empRatePerHr))
+case $empCheck in 
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+salary=$(($empHrs*$empRatePerHr));
